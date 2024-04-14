c9d1fd98d34529611061889749ff885e0ebeb929
Mitesh Tiwari <tiwarimitesh4@gmail.com>
2024-04-14 13:39:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-frontend-hoisting-5pwa-5nhxxykwg.vercel.app/",
 		credentials:true,
 		optionSuccessStatus:200,
 	})
